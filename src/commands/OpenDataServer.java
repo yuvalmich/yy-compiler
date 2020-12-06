ec5d1547f844430430040ec2824e3eecf2745ef2
Yuval <yuvalmich2@gmail.com>
2020-12-06 21:58:31 +0200

    implement  openDataServer command

--- src/commands/OpenDataServer.java
+++ src/commands/OpenDataServer.java
@@ -1,5 +1,17 @@
 package commands;
 
-public class OpenDataServer {
+import interpreter.ClientHandler;
+import interpreter.DataReaderServer;
+import interpreter.MyClientHandler;
+
+public class OpenDataServer implements Command {
+
+	@Override
+	public void execute() {
+		DataReaderServer server = new DataReaderServer();
+		ClientHandler c = new MyClientHandler();
+		
+		server.open(5400, c);
+	}
 
 }
