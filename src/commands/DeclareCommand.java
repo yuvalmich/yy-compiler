731fd3f8f5a8d06a2d1222b3df4d61e2e736014c
Yuval <yuvalmich2@gmail.com>
2020-12-07 23:11:20 +0200

    implement declare command

--- src/commands/DeclareCommand.java
+++ src/commands/DeclareCommand.java
@@ -1,14 +1,24 @@
 package commands;
 
 import java.util.concurrent.Callable;
+import utils.BindValue;
+import utils.VarBindings;
 
-import expressions.Expression;
 
 public class DeclareCommand implements Command {
 
 	@Override
 	public void execute(Callable<String> getNextParam) {
-		// TODO Auto-generated method stub
+		try {
+			String varName = getNextParam.call();
+			
+			VarBindings.programVars.put(varName, new BindValue(0));
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
