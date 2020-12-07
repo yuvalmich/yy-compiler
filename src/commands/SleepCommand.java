f82f727a17129d5f4f362ebe3ab507e69e06e9d0
Yuval <yuvalmich2@gmail.com>
2020-12-07 22:06:13 +0200

    implement sleep command

--- src/commands/SleepCommand.java
+++ src/commands/SleepCommand.java
@@ -1,15 +1,24 @@
 package commands;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
-import expressions.Expression;
+import expressions.Executor;
+
 
 public class SleepCommand implements Command {
 
 	@Override
 	public void execute(Callable<String> getNextParam) {
-		// TODO Auto-generated method stub
-		
+		try {
+			int sleepTime = (int) Executor.calc(getNextParam.call());
+			TimeUnit.SECONDS.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
