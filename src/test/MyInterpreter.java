443b32d1acef14d1d1d1f922a12005b5b3859561
Yuval <yuvalmich2@gmail.com>
2020-12-08 18:01:55 +0200

    implement my interpreter

---
+++ src/test/MyInterpreter.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.Arrays;
+
+import interpreter.Lexer;
+import interpreter.Parser;
+
+public class MyInterpreter {
+
+	public static  int interpret(String[] lines){
+		String convertedCode = String.join("\r\n", lines);
+		
+		
+		Lexer lexer = new Lexer();
+		var expressions = lexer.lexer(convertedCode);
+		
+		System.out.println(Arrays.toString(expressions));
+		
+		Parser parser = new Parser(expressions);
+		
+		parser.parse();
+		
+		return 0;
+	}
+}
